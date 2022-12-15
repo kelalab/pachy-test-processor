691e9593bbfeb05e5208f948513ca93ac4b518c9
mimyllyv <mimyllyv@gmail.com>
2022-12-15 15:03:02 +0200

    Mod

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import os
 
-PFS = '/pfs'
-INPUT  = '/pfs/data'
+INPUT  = '/pfs/images'
 OUTPUT = '/pfs/out'
 
 if __name__ == "__main__":
     print('Input folder content:')
-    print(os.listdir(PFS))
-    print('Environment variables:')
-    print(os.environ)
+    print(os.listdir(INPUT))
